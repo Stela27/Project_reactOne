ab5fc60b79f2600a035925a3979fd44b83847c75
Stela27 <estelahehe99@gmail.com>
2021-10-20 14:47:22 -0500

    Cambio de titulo, componente Signin

--- src/Components/Signin/Signinone.jsx
+++ src/Components/Signin/Signinone.jsx
@@ -8,7 +8,7 @@ const Signinone = () => {
                 <Icon to="/">dolla</Icon>
                 <FormContent>
                     <Form action='#'>
-                        <FormH1>Sign in to your account</FormH1>
+                        <FormH1>Sign in </FormH1>
                         <FormLabel htmlFor='for'>Email</FormLabel>
                         <FormInput type='email' required/>
                         <FormLabel htmlFor='for'>Password</FormLabel>
